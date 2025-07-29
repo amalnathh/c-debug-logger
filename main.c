ece1f9c1931467d603ccc8c26b630f3d78022595
amalnathh <amalnathrn88@gmail.com>
2025-07-29 08:06:54 +0530

    [UPDATE] Examples changed

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include "log.h"
 
 int main() {
-    int temperature = 500;
-    
     // These work in debug builds, disappear in production
-    log_info("System starting...");
-    log_debug("Temperature sensor: %d°C", temperature);
-    
-    if (temperature > 50) {
-        log_error("Overheating detected: %d°C", temperature);
-        log_critical("Fuck you");
-    }
-    
+    log_info("This is an info");
+    log_debug("Debug Var = %d", 55);
+    log_warning("Warning");
+    log_error("Major error");
+    log_critical("Critical");
     return 0;
 }
